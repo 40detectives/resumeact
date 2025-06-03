e8aaf53d33a93126a7f22fa302b6e50739941ced
40detectives <9024463+40detectives@users.noreply.github.com>
2025-06-03 17:11:16 +0200

    us: make the Form View container scrollable

--- src/compositions/form-view/form-view.tsx
+++ src/compositions/form-view/form-view.tsx
@@ -23,16 +23,35 @@ export const FormView: React.FC<Props> = () => {
       />
       <p>Scale: {scale}</p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
-        dolor eveniet molestiae, officiis architecto a dignissimos nulla laborum
-        excepturi omnis illum! Natus beatae quidem voluptas, sed voluptates
-        nostrum id cupiditate.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam fugit
+        reprehenderit a consequuntur explicabo adipisci rem reiciendis sed! Ea
+        quaerat tempora ratione qui cum deserunt perferendis maiores vitae
+        nulla, adipisci ipsum laudantium itaque, quos distinctio deleniti! Quod,
+        possimus eveniet ut delectus voluptatibus modi ea numquam.
       </p>
       <p>
-        Autem nisi ducimus alias placeat eligendi sunt quasi, odio distinctio
-        possimus voluptatum sint nemo sed fugiat tenetur praesentium cumque
-        corporis ea reiciendis quis doloremque quidem ab exercitationem. Iusto,
-        odit rerum!
+        Dolores tenetur modi qui! Laboriosam adipisci facilis hic minus, animi
+        enim numquam. Veritatis optio sed quam. Saepe hic voluptatibus,
+        excepturi accusantium magnam maxime doloribus dolorum quam impedit
+        soluta nam corrupti rem. Voluptatem fugiat incidunt exercitationem
+        consequuntur, earum consectetur ut ratione eveniet. Voluptatibus
+        mollitia molestiae doloribus.
+      </p>
+      <p>
+        Eligendi eos tempora odit, obcaecati architecto sapiente mollitia alias?
+        Totam minima pariatur dolorem vel eligendi beatae voluptatibus numquam
+        quae, doloribus illo obcaecati itaque laboriosam unde, asperiores
+        debitis dicta optio a consequatur tempore, nulla voluptatum similique.
+        Nesciunt laudantium omnis maxime voluptate a rem, tempora officia
+        assumenda?
+      </p>
+      <p>
+        Doloribus suscipit excepturi exercitationem recusandae? Beatae, veniam
+        repudiandae vero voluptatibus aliquid et quasi iusto? Repellat error eum
+        qui dignissimos? Obcaecati odio animi libero harum quidem officia
+        impedit amet. Natus soluta adipisci sed deserunt eum exercitationem,
+        eligendi saepe minus, aliquid delectus officia ut, illum nobis.
+        Consectetur.
       </p>
     </aside>
   );
