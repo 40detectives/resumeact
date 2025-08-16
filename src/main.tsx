325d858cb856157ab77326363bf835d717530b63
40detectives <9024463+40detectives@users.noreply.github.com>
2025-08-16 17:33:50 +0200

    task: make index.css the first stylesheet to load
    This allows to have a consistent way of getting the global css stylesheet (index.css) in dev and prod.
    Because it is now linked from the index.html it would be the first stylesheet in `document.styleSheets` in dev, and it its styles would be the first declared when on prod

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { IconContext } from "react-icons";
 import App from "./App.tsx";
-import "./index.css";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <IconContext.Provider value={{ className: "react-icon" }}>
-      <ThemeProvider theme={"monotone"} palette={["#8a937f"]}>
+      <ThemeProvider theme={"monotone"} palette={["#8a937f", "", "", ""]}>
         <App />
       </ThemeProvider>
     </IconContext.Provider>
