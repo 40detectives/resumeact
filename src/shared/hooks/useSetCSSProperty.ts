7303350f91488e3cbfc0376e661fa673d5c37fba
40detectives <9024463+40detectives@users.noreply.github.com>
2025-09-06 16:43:03 +0200

    feat(hooks): add custom hooks for adding CSS custom props from JS

--- src/shared/hooks/useSetCSSProperty.ts
+++ src/shared/hooks/useSetCSSProperty.ts
@@ -1,22 +1,62 @@
-import { useEffect, useLayoutEffect } from "react";
+import type { ThemePalette } from "@/types/theme-types";
+import type {
+  AlignItems,
+  CSSInheritance,
+  JustifyItems,
+} from "@/types/styleprops-types";
+import { deepEqual } from "fast-equals";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import {
+  useCustomCompareEffect,
+  useCustomCompareMemo,
+} from "use-custom-compare";
+
+interface CSSRuleTypes {
+  "--align-items": AlignItems | CSSInheritance | undefined | null;
+  "--justify-items": JustifyItems | CSSInheritance | undefined | null;
+  "--align-self": AlignItems | CSSInheritance | undefined | null;
+  "--justify-self": JustifyItems | CSSInheritance | undefined | null;
+  "--palette-0": string | undefined | null;
+  "--palette-1": string | undefined | null;
+  "--palette-2": string | undefined | null;
+  "--palette-3": string | undefined | null;
+}
+
+type CSSRuleProperty = keyof CSSRuleTypes;
+
+// type StyleProps = Partial<CSSRuleTypes>;
+type StyleProps = {
+  [K in keyof CSSRuleTypes]?: CSSRuleTypes[K] | null;
+};
+
+/* const tmp = {
+  "--align-items": "center",
+  "--justify-items": "end",
+  "--oopaaa": "center",
+} satisfies StyleProps;
+const obj: StyleProps = tmp; */
+
+type CSSRuleValue = string | null | undefined;
+type CSSRulePriority = "important" | "" | null;
 
 interface CSSRuleObject {
-  property: string;
-  value?: string | null;
-  priority?: "important" | "" | null;
+  property: CSSRuleProperty;
+  value?: CSSRuleValue;
+  priority?: CSSRulePriority;
 }
 
 export const useSetCSSProperty = <T extends HTMLElement>(
   elemRef: React.RefObject<T | null>,
   { property, value, priority }: CSSRuleObject
-): React.RefObject<T | null> => {
+): void => {
   useEffect(() => {
     elemRef.current?.style.setProperty(property, value ?? "", priority ?? "");
   }, [elemRef, property, value, priority]);
-
-  return elemRef;
 };
 
+// !!!!
+// ! delte sync version as there is no need no layout measurement
+// !!!!
 export const useSetCSSPropertySync = <T extends HTMLElement>(
   elemRef: React.RefObject<T | null>,
   { property, value, priority }: CSSRuleObject
@@ -26,4 +66,159 @@ export const useSetCSSPropertySync = <T extends HTMLElement>(
   }, [elemRef, property, value, priority]);
 
   return elemRef;
+};
+// !!!!
+
+export const useSetCSSPropertiesNeedsMemo = <T extends HTMLElement>(
+  elemRef: React.RefObject<T | null>,
+  rules: CSSRuleObject[]
+): void => {
+  useEffect(() => {
+    for (const { property, value, priority } of rules) {
+      elemRef.current?.style.setProperty(property, value ?? "", priority ?? "");
+    }
+  }, [elemRef, rules]);
+};
+
+export const useSetCSSPropertiesAll = <T extends HTMLElement>(
+  elemRef: React.RefObject<T | null>,
+  rules: CSSRuleObject[]
+): void => {
+  useCustomCompareEffect(
+    () => {
+      for (const { property, value, priority } of rules) {
+        elemRef.current?.style.setProperty(
+          property,
+          value ?? "",
+          priority ?? ""
+        );
+      }
+    },
+    [elemRef, rules],
+    (prevDeps, nextDeps) => deepEqual(prevDeps, nextDeps)
+  );
+};
+
+export const useSetCPGridPlaceItems = <T extends HTMLElement>(
+  elemRef: React.RefObject<T | null>,
+  {
+    justifyItems,
+    alignItems,
+  }: { justifyItems: CSSRuleValue; alignItems: CSSRuleValue }
+): void => {
+  useSetCSSProperty(elemRef, {
+    property: "--justify-items",
+    value: justifyItems,
+    priority: "",
+  });
+  useSetCSSProperty(elemRef, {
+    property: "--align-items",
+    value: alignItems,
+    priority: "",
+  });
+};
+
+export const useSetCPGridPlaceSelf = <T extends HTMLElement>(
+  elemRef: React.RefObject<T | null>,
+  {
+    justifySelf,
+    alignSelf,
+  }: { justifySelf: CSSRuleValue; alignSelf: CSSRuleValue }
+): void => {
+  useSetCSSProperty(elemRef, {
+    property: "--justify-self",
+    value: justifySelf,
+    priority: "",
+  });
+  useSetCSSProperty(elemRef, {
+    property: "--align-self",
+    value: alignSelf,
+    priority: "",
+  });
+};
+
+export const useSetCPPalette = <T extends HTMLElement>(
+  elemRef: React.RefObject<T | null>,
+  palette?: ThemePalette
+): void => {
+  useSetCSSProperty(elemRef, {
+    property: "--palette-0",
+    value: palette?.[0],
+    priority: "",
+  });
+  useSetCSSProperty(elemRef, {
+    property: "--palette-1",
+    value: palette?.[1],
+    priority: "",
+  });
+  useSetCSSProperty(elemRef, {
+    property: "--palette-2",
+    value: palette?.[2],
+    priority: "",
+  });
+  useSetCSSProperty(elemRef, {
+    property: "--palette-3",
+    value: palette?.[3],
+    priority: "",
+  });
+};
+
+export function usePropDiff<T extends object>(currentProps: T): Partial<T> {
+  const prevPropsRef = useRef<T>({} as T);
+
+  const diff = useCustomCompareMemo(
+    () => {
+      const { current: prevProps } = prevPropsRef;
+
+      const allKeys = new Set<keyof T>([
+        ...(Object.keys(prevProps) as (keyof T)[]),
+        ...(Object.keys(currentProps) as (keyof T)[]),
+      ]);
+
+      const currentDiff: T = {} as T;
+
+      for (const key of allKeys) {
+        const prevHas = key in prevProps;
+        const currHas = key in currentProps;
+        const valueChanged = prevProps[key] !== currentProps[key];
+
+        if (!prevHas && currHas) {
+          currentDiff[key] = currentProps[key]; // prop added
+        } else if (prevHas && !currHas) {
+          currentDiff[key] = currentProps[key]; // prop deleted, we maintain it with undefined
+        } else if (valueChanged) {
+          currentDiff[key] = currentProps[key]; // value updated
+        }
+      }
+
+      prevPropsRef.current = currentProps;
+      return currentDiff;
+    },
+    [currentProps],
+    (prevDeps, nextDeps) => deepEqual(prevDeps, nextDeps)
+  );
+  return diff;
+}
+
+export function useStylePropDiff(currentProps: StyleProps) {
+  return usePropDiff(currentProps);
+}
+
+export const useSetCSSCustomProperties = <T extends HTMLElement>(
+  elemRef: React.RefObject<T | null>,
+  props: StyleProps
+): void => {
+  const changes = useStylePropDiff(props);
+
+  useCustomCompareEffect(
+    () => {
+      for (const key of Object.keys(changes) as (keyof StyleProps)[]) {
+        const [value, priority] = String(changes[key] ?? "").split("!");
+        elemRef.current?.style.setProperty(key, value, priority);
+        console.log(`I'm updating ${key} with value ${value}`);
+      }
+    },
+    [elemRef, changes],
+    (prevDeps, nextDeps) => deepEqual(prevDeps, nextDeps)
+  );
 };
